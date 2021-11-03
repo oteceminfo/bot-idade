fd0856f35141161cb649582227adeea973e2506d
O Técnico em Informática <93675150+oteceminfo@users.noreply.github.com>
2021-11-03 16:44:10 -0300

    Create robot.sh

---
+++ robot.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo - n "Qual é a sua idade? "
+read idade
+
+if [[ idade -lt 18 ]]; then
+  echo "Não foi possível efetuar o seu cadastro."
+elif [[ idade -ge 18 ]]; then
+  echo "Cadastro efetuado com sucesso!"
+else
+  echo "Não conseguimos entender. Digite somente números."
+fi
