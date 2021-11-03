d258c526ebde957b53adc850a4cf95cc04473f82
O Técnico em Informática <93675150+oteceminfo@users.noreply.github.com>
2021-11-03 16:46:54 -0300

    Update robot.sh

--- robot.sh
+++ robot.sh
@@ -3,9 +3,9 @@
 echo - n "Qual é a sua idade? "
 read idade
 
-if [[ idade -lt 18 ]]; then
+if [[ $idade -lt 18 ]]; then
   echo "Não foi possível efetuar o seu cadastro."
-elif [[ idade -ge 18 ]]; then
+elif [[ $idade -ge 18 ]]; then
   echo "Cadastro efetuado com sucesso!"
 else
   echo "Não conseguimos entender. Digite somente números."
